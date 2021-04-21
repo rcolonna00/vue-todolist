c0c1dc62a7b269018fcdfa20fe85a1e5ecda6213
Rocco Colonna <colonna.rocco2000@gmail.com>
2021-04-21 18:26:09 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            userNewTodo: '',
             todos: [
                 'Fare i compiti',
                 'Fare la spesa',
@@ -9,7 +10,19 @@ var app = new Vue(
             ]
         },
         methods: {
-
+            // Add new todo in todos array
+            addNewTodo() {
+                // Si avvia la funzione solo se 
+                // l'utente scrive qualcosa
+                if ( this.userNewTodo.length > 0 ) {
+                    this.todos.push(this.userNewTodo)
+                    this.userNewTodo = '';
+                }
+            },
+            //Rimuove un elemento dall'array todo
+            deleteTodo(index) {
+                this.todos.splice(index, 1)
+            }
         }
     }
 )
