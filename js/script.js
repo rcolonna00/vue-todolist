6422e24958b76970a43381221cd976da1db1ebb9
Rocco Colonna <colonna.rocco2000@gmail.com>
2021-04-21 17:18:02 +0200

    html - css

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            todos: [
+                'Fare i compiti',
+                'Fare la spesa',
+                'Fare il bucato'
+            ]
+        },
+        methods: {
+
+        }
+    }
+)
